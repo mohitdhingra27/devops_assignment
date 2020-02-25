19c74b59d7b5386a2522aea3146ac936bd07d175
mohitdhingra27 <mohitdhingra27@yahoo.com>
2020-02-25 15:00:39 +0100

    added variable file

---
+++ assignment_1/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr_map" {
+  default = {
+    vnet    = ""
+    etcd    = ""
+    masters = ""
+    nodes   = ""
+  }
+}
+
+variable "id" {
+}
+
+variable "location" {
+}
+
+variable "tags" {
+}
